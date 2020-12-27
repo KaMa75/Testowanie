afefab9d7e25759d494ce905b62a3e7c4a213bf1
KaMa75 <karolmarszalek@o2.pl>
2020-12-27 22:07:53 +0100

    Counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import Counter from './components/Counter';
 import './App.css';
+
+const startCount = 99;
 
 function App() {
   return (
     <div className="App">
-      test
+      <Counter start={startCount} />
+      {/* <Counter /> */}
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,103 @@
+import React,{Component} from 'react';
+
+class Counter extends Component {
+
+    state = {
+        count: this.props.start,
+        inputValue: ''
+    }
+
+    handleAddCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    handleSubCount = () => {
+        this.setState({
+            count: this.state.count - 1
+        });
+    }
+
+    handleResetCount = () => {
+        this.setState({
+            count: this.props.start
+        });
+    }
+
+    handleSetCount = () => {
+        const {inputValue} = this.state;
+        this.setState({
+            count: inputValue ? inputValue : this.props.start
+        });
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            inputValue: parseInt(e.target.value)
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    render() {
+        return(
+            <div style={{padding: 50}}>
+                <h3>Stan licznika:</h3>
+
+                <div>
+                    <button
+                        className="button-add"
+                        onClick={this.handleAddCount}
+                    >
+                        +
+                    </button>
+                    <button
+                        className="button-sub"
+                        onClick={this.handleSubCount}
+                    >
+                        -
+                    </button>
+                </div>
+
+                <h2
+                    className="counter-value"
+                >
+                    {this.state.count}
+                </h2>
+
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input
+                            type="number"
+                            value={this.state.inputValue}
+                            onChange={this.handleOnChange}
+                        />
+                    </div>
+                    <div>
+                        <button
+                            className="button-set"
+                            onClick={this.handleSetCount}
+                        >
+                            Set
+                        </button>
+                        <button
+                            className="button-reset"
+                            onClick={this.handleResetCount}
+                        >
+                            Reset
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+Counter.defaultProps = {
+    start: 0
+}
+
+export default Counter;
