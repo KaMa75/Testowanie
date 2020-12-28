456ee1bf49cd1b569a091a0a3bb39ec09d157134
KaMa75 <karolmarszalek@o2.pl>
2020-12-28 14:09:59 +0100

    Counter component test scenario

---
+++ src/components/Counter.test.js
@@ -0,0 +1,31 @@
+import Counter from './Counter';
+
+describe('Test Counter component', () => {
+    it('Should count state equal 0 when doesn\'t start prop', () => {
+
+    });
+
+    it('Should count state equal start prop value', () => {
+
+    });
+
+    it('Should be add and subtract buttons', () => {
+
+    });
+
+    it('Should increment count state after click add button', () => {
+
+    });
+
+    it('Should decrement count state after click subtract button', () => {
+
+    });
+
+    it('Should count state equal input value after click set button', () => {
+
+    });
+
+    it('Should count state equal start prop after click reset button', () => {
+
+    });
+});
